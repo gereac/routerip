0f7455a8c076fde557903f7e4658f8315943fe45
gereac <gereac@0a2210ea-fa34-3e4b-9b3f-54a2521e6e16>
2012-07-17 20:02:36 +0000

    first commit

---
+++ trunk/RouterIP/src/com/gcsf/routerip/IApplicationConstants.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gcsf.routerip;
+
+/**
+ *
+ * @author Virginia
+ */
+public interface IApplicationConstants {
+    
+    public static final String KEY_AUTHENTICATIONURL = "authUrl";
+    public static final String KEY_USERNAME = "usr";
+    public static final String KEY_PASSWORD = "pswd";
+    
+    public static final String DCPPCONFIGURATIONFILE = "DCPlusPlus.xml";
+    
+    public static final String AUTHENTICATIONFILE = ".cfgRouterIP";
+    
+    public static final int MINVALUE_PROGRESSBAR = 0;
+    
+    public static final int MAXVALUE_PROGRESSBAR = 100;
+    
+    public static final int READ_TIMEOUT = 10000;
+    
+    public static final int CONNECT_TIMEOUT = 10000;
+}
